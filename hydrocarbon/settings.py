863bdd7da89174c28ab70b3fe34d71e051586827
JuneHyeon Bae <devunt@gmail.com>
2014-12-09 19:24:54 +0900

    Add django.core.context_processors.request to TEMPLATE_CONTEXT_PROCESSORS

--- hydrocarbon/settings.py
+++ hydrocarbon/settings.py
@@ -74,6 +74,18 @@ DATABASES = {
     }
 }
 
+# Template
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 LANGUAGES = (
