d8af78447f286ad07ad0736d4202e0becd0dd319
JuneHyeon Bae <devunt@gmail.com>
2014-12-09 19:15:41 +0900

    Add UserProfile model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    nick = models.CharField(max_length=16)
+
+    def __str__(self):
+        return str(self.user)
