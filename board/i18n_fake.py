e014eb4b149b4d36d316d18aa3f97f993fb143b8
JuneHyeon Bae <devunt@gmail.com>
2014-12-10 20:40:30 +0900

    Add fake i18n file

---
+++ board/i18n_fake.py
@@ -0,0 +1,11 @@
+_ = lambda s: s
+
+_('Username')
+_('username')
+_('E-mail')
+_('Password')
+_('Password (again)')
+_('The two password fields didn\'t match.')
+_('New password')
+_('New password confirmation')
+_('Old password')
