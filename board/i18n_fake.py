0fe73c2474f46eaa609e7fcba0c31e4958f21d56
JuneHyeon Bae <devunt@gmail.com>
2014-12-15 08:45:07 +0900

    Fix timesince filter translation

--- board/i18n_fake.py
+++ board/i18n_fake.py
@@ -11,6 +11,16 @@ _('New password')
 _('New password confirmation')
 _('Old password')
 _('Please enter a correct %(username)s and password. Note that both fields may be case-sensitive.')
-ungettext_lazy(
+
+ungettext('%d year', '%d years')
+ungettext('%d month', '%d months')
+ungettext('%d week', '%d weeks')
+ungettext('%d day', '%d days')
+ungettext('%d hour', '%d hours')
+ungettext('%d minute', '%d minutes')
+_('0 minutes')
+_(', ')
+
+ungettext(
         'Ensure this value has at least %(limit_value)d character (it has %(show_value)d).',
         'Ensure this value has at least %(limit_value)d characters (it has %(show_value)d).')
