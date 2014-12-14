c82c2b984dce9dd772e4160763e61b728236192b
JuneHyeon Bae <devunt@gmail.com>
2014-12-15 08:59:54 +0900

    Add split_first filter

--- board/templatetags/hc.py
+++ board/templatetags/hc.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 
 register = template.Library()
@@ -7,3 +8,8 @@ register = template.Library()
 def label_as_placeholder(field):
     field.field.widget.attrs.update({'placeholder': field.label})
     return field
+
+@register.filter
+@stringfilter
+def split_first(value):
+    return value.split()[0]
