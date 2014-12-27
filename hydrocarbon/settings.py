1fc09556147d9e2eba2a50c8e30f356f4027807d
JuneHyeon Bae <devunt@gmail.com>
2014-12-27 13:38:56 +0900

    List of nodes should not include schema information

--- hydrocarbon/settings.py
+++ hydrocarbon/settings.py
@@ -169,7 +169,7 @@ SUMMERNOTE_CONFIG = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
+        'URL': '127.0.0.1:9200',
         'INDEX_NAME': 'haystack',
     },
 }
