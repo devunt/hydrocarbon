d380ae026cc6b9354e7bfe314528f138e4cf326f
Junehyeon Bae <devunt@gmail.com>
2015-07-25 12:45:14 +0900

    Move reflection logic to search_indexes.py

--- board/apps.py
+++ board/apps.py
@@ -2,7 +2,6 @@ import django
 
 from django.apps import AppConfig
 from django.conf import settings
-from importlib import import_module
 
 from board.pagination import HCPaginator
 
@@ -15,6 +14,3 @@ class BoardConfig(AppConfig):
         import board.signals
         django.core.paginator.Paginator = HCPaginator
         settings.FROALA_EDITOR_OPTIONS_COMMENT['placeholder'] = str(settings.FROALA_EDITOR_OPTIONS_COMMENT['placeholder'])
-        module, cls = settings.SEARCH_INDEX_CLASS.rsplit('.', maxsplit=1)
-        module = import_module(module)
-        settings.SEARCH_INDEX_CLASS = getattr(module, cls)
